45130efe26a6982fef2906ed778067bac98b0225
王良 <841369634@qq.com>
2021-06-23 12:55:07 +0000

    feature: StringUtils中，添加的判空方法。(#1)

--- core/src/main/java/org/wangliang181230/core/util/StringUtils.java
+++ core/src/main/java/org/wangliang181230/core/util/StringUtils.java
@@ -6,4 +6,23 @@ package org.wangliang181230.core.util;
  * @author 王良
  */
 public class StringUtils {
+
+	public static boolean isEmpty(String str) {
+		return str == null || str.isEmpty();
+	}
+
+	public static boolean isBlank(String str) {
+		if (isEmpty(str)) {
+			return true;
+		}
+
+		for (int i = 0; i < str.length(); ++i) {
+			if (Character.isWhitespace(str.charAt(i))) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
 }
