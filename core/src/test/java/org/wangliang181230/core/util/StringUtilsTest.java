f1a26ac76934f13ee7c21cc45e7ab4fe7bda0644
王良 <841369634@qq.com>
2021-06-23 19:33:45 +0800

    test: 测试百度效率云。

---
+++ core/src/test/java/org/wangliang181230/core/util/StringUtilsTest.java
@@ -0,0 +1,18 @@
+package org.wangliang181230.core.util;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author 王良/wangliang@jyhk.com
+ * @version 1.0
+ * @since 2021/6/23 19:20
+ */
+public class StringUtilsTest {
+
+	@Test
+	void test() {
+		System.out.println("故意抛异常");
+		throw new RuntimeException("故意抛异常");
+	}
+
+}
