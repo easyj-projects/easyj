9b60afd3eee0dabd153a39524fcacc33a1faabd0
王良 <841369634@qq.com>
2021-07-13 04:31:06 +0800

    feature: 添加`QueryStringUtils`和`QueryStringHttpServletRequestWrapper`两个类。

---
+++ web-parent/web-core/src/main/java/icu/easyj/web/util/QueryStringUtils.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package icu.easyj.web.util;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang3.ArrayUtils;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
+import org.springframework.util.StringUtils;
+
+/**
+ * 查询参数串工具类
+ *
+ * @author wangliang181230
+ */
+public abstract class QueryStringUtils {
+
+	/**
+	 * 解析查询参数串
+	 *
+	 * @param queryString 查询参数串
+	 * @return parameterMap 查询参数Map
+	 */
+	@NonNull
+	public static Map<String, String[]> parse(@Nullable String queryString) {
+		// 如果为空，则返回空的Map
+		if (!StringUtils.hasLength(queryString)) {
+			return new HashMap<>(0);
+		}
+
+		String[] paramArr = queryString.split("&");
+		Map<String, String[]> parameterMap = new HashMap<>(paramArr.length);
+
+		int idx;
+		String key, value;
+		String[] values;
+		for (String param : paramArr) {
+			if (!StringUtils.hasText(param)) {
+				continue;
+			}
+
+			idx = param.indexOf("=");
+			if (idx >= 0) {
+				key = idx == 0 ? "" : param.substring(0, idx);
+				value = param.substring(idx + 1);
+
+				try {
+					value = URLDecoder.decode(value, StandardCharsets.UTF_8.name());
+				} catch (UnsupportedEncodingException ignore) {
+					// do nothing
+				}
+
+				if (parameterMap.containsKey(key)) {
+					values = ArrayUtils.addAll(parameterMap.get(key), value);
+				} else {
+					values = new String[]{value};
+				}
+			} else {
+				key = param;
+				values = new String[]{""};
+			}
+
+			parameterMap.put(key, values);
+		}
+
+		return parameterMap;
+	}
+
+}
---
+++ web-parent/web-core/src/main/java/icu/easyj/web/wrapper/QueryStringHttpServletRequestWrapper.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package icu.easyj.web.wrapper;
+
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+import icu.easyj.web.util.QueryStringUtils;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
+import org.springframework.util.StringUtils;
+
+/**
+ * 查询参数串 HttpServletRequest包装类
+ *
+ * @author wangliang181230
+ */
+public class QueryStringHttpServletRequestWrapper extends HttpServletRequestWrapper {
+
+	/**
+	 * 查询参数串
+	 */
+	private final String queryString;
+
+	/**
+	 * 解析后的查询参数缓存
+	 */
+	private Map<String, String[]> parameterMap;
+
+	/**
+	 * 构造函数
+	 *
+	 * @param request     请求
+	 * @param queryString 查询参数串
+	 */
+	public QueryStringHttpServletRequestWrapper(HttpServletRequest request, String queryString) {
+		super(request);
+		this.queryString = queryString == null ? "" : queryString;
+	}
+
+	@NonNull
+	@Override
+	public String getQueryString() {
+		return queryString;
+	}
+
+	@NonNull
+	@Override
+	public Map<String, String[]> getParameterMap() {
+		if (this.parameterMap == null) {
+			this.parameterMap = QueryStringUtils.parse(this.queryString);
+		}
+		return this.parameterMap;
+	}
+
+	@NonNull
+	@Override
+	public Enumeration<String> getParameterNames() {
+		return Collections.enumeration(getParameterMap().keySet());
+	}
+
+	@Nullable
+	@Override
+	public String[] getParameterValues(String name) {
+		return getParameterMap().get(name);
+	}
+
+	@Nullable
+	@Override
+	public String getParameter(String name) {
+		String[] values = getParameterValues(name);
+		if (values == null) {
+			return null;
+		} else if (values.length == 0) {
+			return "";
+		} else if (values.length == 1) {
+			return values[0];
+		} else {
+			for (String value : values) {
+				if (StringUtils.hasText(value)) {
+					return value;
+				}
+			}
+			return values[0];
+		}
+	}
+}
