489e04b152220ef9588fd73b5bf67681b1c4edd3
王良 <841369634@qq.com>
2021-10-12 20:18:35 +0800

    optimize: 移除不需要的构造函数

--- test/src/main/java/icu/easyj/test/exception/TestException.java
+++ test/src/main/java/icu/easyj/test/exception/TestException.java
@@ -23,10 +23,6 @@ package icu.easyj.test.exception;
 public class TestException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
-	public TestException() {
-		super();
-	}
-
 	public TestException(String message) {
 		super(message);
 	}
