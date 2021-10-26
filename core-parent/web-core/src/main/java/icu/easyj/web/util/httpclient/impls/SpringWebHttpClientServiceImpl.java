9ac5bffef17fd908cc01f14ef5fbc70115cbd1dc
王良 <841369634@qq.com>
2021-10-27 00:11:33 +0800

    rename package 'impl' -> 'impls'

--- core-parent/web-core/src/main/java/icu/easyj/web/util/httpclient/impls/SpringWebHttpClientServiceImpl.java
+++ core-parent/web-core/src/main/java/icu/easyj/web/util/httpclient/impls/SpringWebHttpClientServiceImpl.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package icu.easyj.web.util.httpclient.impl;
+package icu.easyj.web.util.httpclient.impls;
 
 import java.util.Map;
 
