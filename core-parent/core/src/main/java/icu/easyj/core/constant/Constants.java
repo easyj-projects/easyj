7b01f401423886a2002a55583934a92d45472451
王良 <841369634@qq.com>
2021-11-03 18:38:06 +0800

    release: 发布0.2.1版本。

--- core-parent/core/src/main/java/icu/easyj/core/constant/Constants.java
+++ core-parent/core/src/main/java/icu/easyj/core/constant/Constants.java
@@ -27,7 +27,7 @@ public interface Constants {
 
 	String PREFIX = "easyj";
 
-	String VERSION = "0.2.1-SNAPSHOT";
+	String VERSION = "0.2.1";
 
 	VersionInfo VERSION_INFO = VersionUtils.parse(VERSION);
 }
