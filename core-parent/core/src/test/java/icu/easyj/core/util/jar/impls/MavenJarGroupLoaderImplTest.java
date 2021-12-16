1c1d68cc47624a4515072bdd153fd9b278955978
王良 <841369634@qq.com>
2021-12-16 13:11:24 +0800

    optimize: 低版本spring-boot，@Test 改为junit4的。

--- core-parent/core/src/test/java/icu/easyj/core/util/jar/impls/MavenJarGroupLoaderImplTest.java
+++ core-parent/core/src/test/java/icu/easyj/core/util/jar/impls/MavenJarGroupLoaderImplTest.java
@@ -15,8 +15,8 @@
  */
 package icu.easyj.core.util.jar.impls;
 
+import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 
 /**
  * {@link MavenJarGroupLoaderImpl} 测试类
