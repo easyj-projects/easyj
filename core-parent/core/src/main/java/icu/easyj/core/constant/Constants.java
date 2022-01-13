98724e66d8530878190476313000b6f7113606d3
王良 <841369634@qq.com>
2022-01-13 12:14:00 +0800

    release: 发布0.2.6版本。

--- core-parent/core/src/main/java/icu/easyj/core/constant/Constants.java
+++ core-parent/core/src/main/java/icu/easyj/core/constant/Constants.java
@@ -27,7 +27,7 @@ public interface Constants {
 
 	String PREFIX = "easyj";
 
-	String VERSION = "0.2.6-SNAPSHOT";
+	String VERSION = "0.2.6";
 
 	VersionInfo VERSION_INFO = VersionUtils.parse(VERSION);
 }
