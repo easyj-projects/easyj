fd4024a128f680bf18d343d2f968e997944d93ba
王良 <841369634@qq.com>
2022-02-10 14:07:16 +0800

    version: 修改版本号为0.2.8-SNAPSHOT。

--- core-parent/core/src/main/java/icu/easyj/core/constant/Constants.java
+++ core-parent/core/src/main/java/icu/easyj/core/constant/Constants.java
@@ -27,7 +27,7 @@ public interface Constants {
 
 	String PREFIX = "easyj";
 
-	String VERSION = "0.2.7";
+	String VERSION = "0.2.8-SNAPSHOT";
 
 	VersionInfo VERSION_INFO = VersionUtils.parse(VERSION);
 }
