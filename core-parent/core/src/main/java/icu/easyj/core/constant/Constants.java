6dd05b3015495ff1cd250a862d25d49bd6748031
王良 <841369634@qq.com>
2022-04-06 22:58:00 +0800

    release: 发布0.3.8版本。(新增jwt、login模块)

--- core-parent/core/src/main/java/icu/easyj/core/constant/Constants.java
+++ core-parent/core/src/main/java/icu/easyj/core/constant/Constants.java
@@ -27,7 +27,7 @@ public interface Constants {
 
 	String PREFIX = "easyj";
 
-	String VERSION = "0.3.8-SNAPSHOT";
+	String VERSION = "0.3.8";
 
 	VersionInfo VERSION_INFO = VersionUtils.parse(VERSION);
 }
