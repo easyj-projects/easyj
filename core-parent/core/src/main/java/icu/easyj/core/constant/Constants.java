c10f440b8cfec618893ff6fc4f7aae5c9c1f9051
王良 <841369634@qq.com>
2022-05-26 12:45:01 +0800

    release: 发布0.4.3版本。(增强easyj-maven-plugin插件的simplify-pom功能，使其更加智能)

--- core-parent/core/src/main/java/icu/easyj/core/constant/Constants.java
+++ core-parent/core/src/main/java/icu/easyj/core/constant/Constants.java
@@ -27,7 +27,7 @@ public interface Constants {
 
 	String PREFIX = "easyj";
 
-	String VERSION = "0.4.3-SNAPSHOT";
+	String VERSION = "0.4.3";
 
 	VersionInfo VERSION_INFO = VersionUtils.parse(VERSION);
 }
