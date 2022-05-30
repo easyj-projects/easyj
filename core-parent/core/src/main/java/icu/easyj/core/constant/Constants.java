cb7a3c4c8511e277c7cc47b563bf86f8053e3408
王良 <841369634@qq.com>
2022-05-30 19:25:52 +0800

    release: 发布0.5.4版本。(新增print-project插件; simplify-pom优化dependencyManagement的处理）

--- core-parent/core/src/main/java/icu/easyj/core/constant/Constants.java
+++ core-parent/core/src/main/java/icu/easyj/core/constant/Constants.java
@@ -27,7 +27,7 @@ public interface Constants {
 
 	String PREFIX = "easyj";
 
-	String VERSION = "0.5.4-SNAPSHOT";
+	String VERSION = "0.5.4";
 
 	VersionInfo VERSION_INFO = VersionUtils.parse(VERSION);
 }
