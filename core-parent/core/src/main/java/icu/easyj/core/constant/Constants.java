02d3c5fe67db64cac002c7720d1072c3c0e1e639
王良 <841369634@qq.com>
2022-05-30 15:41:08 +0800

    release: 发布0.5.3版本。(修复simplify-pom插件，导致依赖的依赖丢失问题）

--- core-parent/core/src/main/java/icu/easyj/core/constant/Constants.java
+++ core-parent/core/src/main/java/icu/easyj/core/constant/Constants.java
@@ -27,7 +27,7 @@ public interface Constants {
 
 	String PREFIX = "easyj";
 
-	String VERSION = "0.5.3-SNAPSHOT";
+	String VERSION = "0.5.3";
 
 	VersionInfo VERSION_INFO = VersionUtils.parse(VERSION);
 }
