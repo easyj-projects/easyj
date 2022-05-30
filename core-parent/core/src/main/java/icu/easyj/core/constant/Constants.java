4b10a9b1d5f2c8dfd9136312bc27e00b67462634
王良 <841369634@qq.com>
2022-05-30 17:34:27 +0800

    version: 修改版本号为0.5.4-SNAPSHOT。

--- core-parent/core/src/main/java/icu/easyj/core/constant/Constants.java
+++ core-parent/core/src/main/java/icu/easyj/core/constant/Constants.java
@@ -27,7 +27,7 @@ public interface Constants {
 
 	String PREFIX = "easyj";
 
-	String VERSION = "0.5.3";
+	String VERSION = "0.5.4-SNAPSHOT";
 
 	VersionInfo VERSION_INFO = VersionUtils.parse(VERSION);
 }
