013e2a25fac396cdaa7d0bd5cc3d1f086f49734e
王良 <841369634@qq.com>
2023-12-01 16:30:48 +0800

    optimize: mysql中的sql脚本，序列名长度从50加长到255。

--- src/script/db/sequence/mysql/mysql__create_sequence-table_and_functions.sql
+++ src/script/db/sequence/mysql/mysql__create_sequence-table_and_functions.sql
@@ -1,15 +1,15 @@
 -- 建表语句
 CREATE TABLE `easyj_sys_sequence`
 (
-    `name`            varchar(50) NOT NULL           COMMENT '序列的名字，唯一',
-    `current_value`   bigint      NOT NULL           COMMENT '当前的值',
-    `increment_value` tinyint     NOT NULL DEFAULT 1 COMMENT '步长，默认为1',
+    `name`            varchar(255) NOT NULL           COMMENT '序列的名字，唯一',
+    `current_value`   bigint       NOT NULL           COMMENT '当前的值',
+    `increment_value` tinyint      NOT NULL DEFAULT 1 COMMENT '步长，默认为1',
     PRIMARY KEY (`name`) USING BTREE
 ) COMMENT = '公共的序列表';
 
 
 -- 获取当前序列值的函数
-CREATE FUNCTION func_currval (seq_name varchar(50))
+CREATE FUNCTION func_currval (seq_name varchar(255))
     RETURNS bigint
     DETERMINISTIC
 BEGIN
@@ -33,7 +33,7 @@ END;
 
 
 -- 获取下个序列值的函数
-CREATE FUNCTION func_nextval (seq_name varchar(50))
+CREATE FUNCTION func_nextval (seq_name varchar(255))
     RETURNS bigint
     DETERMINISTIC
 BEGIN
@@ -46,7 +46,7 @@ END;
 
 
 -- 设置序列值的函数（起到纠正当前值的作用）
-CREATE FUNCTION func_setval (seq_name varchar(50), val bigint)
+CREATE FUNCTION func_setval (seq_name varchar(255), val bigint)
     RETURNS bigint
     DETERMINISTIC
 BEGIN
